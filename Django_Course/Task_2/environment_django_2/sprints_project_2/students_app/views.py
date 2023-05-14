be15b45c3a12f1522b9ba84c1ecd2c6d6a45c292
abd0Samy <abdo_s.1997@live.com>
2023-05-14 17:20:02 +0300

    Django Task_2

---
+++ Django_Course/Task_2/environment_django_2/sprints_project_2/students_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def Hello(request):
+	template=loader.get_template('show.html')
+	return HttpResponse(template.render())
+	#return HttpResponse("Welcome to our site")
