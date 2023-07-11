3a76dcbcd7f856b95828943906e38ced61526588
abd0Samy <abdo_s.1997@live.com>
2023-07-11 21:43:07 +0300

    Update outputs.tf

--- Terraform/Task_2/outputs.tf
+++ Terraform/Task_2/outputs.tf
@@ -1,7 +1,7 @@
 output "public_instance_ip" {
-	value = aws_instance.public_instance1.public_ip
+	value = aws_instance.instance["public_subnet"].public_ip
 }
 
 output "private_instance_ip" {
-	value = aws_instance.private_instance1.private_ip
+	value = aws_instance.instance["private_subnet"].private_ip
 }
